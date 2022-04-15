ea85e444715dcd9fdc727f40005441865681dac9
OskarHowe <oskarhowe@yahoo.de>
2022-04-15 10:56:20 +0200

    updated ingredient model

--- src/Model/ingredientModel.js
+++ src/Model/ingredientModel.js
@@ -3,6 +3,12 @@ import { Schema, model } from "mongoose";
 const ingredientModel = new Schema({
   name: { type: String, required: true, unique: true },
   imageUrl: { type: String },
+  idsRecette: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Recette",
+    },
+  ],
 });
 
 module.exports = model("Ingredient", ingredientModel);
